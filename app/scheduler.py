f69aacf8119d1923e7ad87d13cd422e5a39abb4c
Vivek Singh Rathore <kakashi@Viveks-MacBook-Pro.local>
2021-05-15 14:27:49 +0530

    added missfire_grace_time for crons

--- app/scheduler.py
+++ app/scheduler.py
@@ -3,5 +3,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def scheduler(job):
     scheduler = BackgroundScheduler()
-    scheduler.add_job(job, "interval", seconds=60)
+    scheduler.add_job(job, "interval", seconds=60, misfire_grace_time=300)
     scheduler.start()
