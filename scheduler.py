a8a362be305fdc061c2ab4c2ab84151cc0d5db25
Vivek Singh Rathore <rathoreviveksingh960@gmail.com>
2021-06-29 20:11:00 +0530

    Delete scheduler.py
    deleted unused file

--- scheduler.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-
-
-def scheduler(job):
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(job, "interval", seconds=60, misfire_grace_time=300)
-    scheduler.start()
